c40edf79f28f7f9b8c2c09b0eadc527bf994c22d
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-02-28 20:06:57 +0200

    Add project files.

---
+++ 01_Intro/01_Intro.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    cout << "Hello World!\n";
+
+    cout << "Any text to write\n";
+    cout << "Any text to write";
+    cout << "Any text\t to write";
+    cout << "Any text to \twrite";
+    cout << "Any text to write";
+    cout << "Any text to write";
+    cout << "Any text to write\n";
+
+
+
+
+
+
+}
+
