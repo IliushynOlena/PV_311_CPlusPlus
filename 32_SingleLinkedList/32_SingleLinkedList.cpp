f6184703542e943f2e1a0a2b97bda679b7604383
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-04-29 20:03:51 +0300

    Single linked List

---
+++ 32_SingleLinkedList/32_SingleLinkedList.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+struct Node 
+{
+    int value;
+    Node *next;
+    Node(int value, Node * next):value(value),next(next){}
+
+};
+
+class List
+{
+private:
+    Node* head;
+public:
+    List()
+    {
+        head = nullptr;
+    }
+    void AddToHead(int value)
+    {
+        Node* newNode = new Node(value, head);
+
+        //newNode->value = value;
+        //newNode->next = head;
+        head = newNode;
+    }
+    void Print()
+    {
+        Node* current = head;
+        while (current!= nullptr)
+        {
+            cout << current->value << " ";
+            current = current->next;
+        }  
+        cout << endl;
+    }
+    ~List()
+    {
+        Node* current = nullptr;
+        while (head != nullptr)
+        {
+            current = head;
+            head = head->next;
+            delete current;
+        }
+    }
+    void AddToTail(int value)
+    {
+        Node* newNode = new Node(value, nullptr);
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else 
+        {
+            Node* current = head;
+            while (current->next != nullptr)
+            {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+       
+    }
+    void DeleteFromTail()
+    {
+        if (head == nullptr)return;
+        if (head->next == nullptr)
+        {
+            delete head;
+            head = nullptr;
+        }
+        else
+        {
+            Node* current = head;
+            while (current->next->next != nullptr)
+            {
+                current = current->next;
+            }
+            delete current->next;
+            current->next = nullptr;
+        }     
+    }
+    int GetElementByPosition(int pos)
+    {
+        Node* current = head;
+        int i = 1;
+        while (current != nullptr)
+        {
+            if (pos == i)return current->value; 
+            current = current->next;
+            i++;
+        }
+        return 0;
+    }
+
+};
+
+int main()
+{
+    List list;
+    for (int i = 0; i < 5; i++)
+    {
+        list.AddToHead(i);
+    }
+    list.AddToTail(100);
+    list.AddToTail(200);
+    list.AddToTail(300);
+    list.Print();
+    cout << "Element [2] = " << list.GetElementByPosition(2) << endl;
+    cout << "Element [5] = " << list.GetElementByPosition(5) << endl;
+    cout << "Element [100] = " << list.GetElementByPosition(100) << endl;
+
+    list.DeleteFromTail();
+    //list.DeleteFromTail();
+    list.Print();
+}
+
